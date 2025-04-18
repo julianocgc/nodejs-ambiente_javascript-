3f90b8da3bf45f5cb85d0df8bbc7d94ac1abbafe
Juliano <72839366+julianocgc@users.noreply.github.com>
2025-04-18 14:44:11 -0300

    Envio de arquivos
    Envio de arquivos Node.js.

---
+++ teste/calc.js
@@ -0,0 +1,8 @@
+var SomaFunc = require("./somar");
+var SubFunc = require("./subtrair");
+var MultiFunc = require("./multiplicar");
+var DivFunc = require("./dividir");
+
+console.log(SomaFunc(5, 2));
+console.log(SubFunc(10, 5));
+console.log(MultiFunc(30, 20));
